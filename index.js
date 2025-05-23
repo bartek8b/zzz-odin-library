ce0e0a823a7f4b6d4ac77fd17201cae41ccd6594
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-05-23 20:41:41 +0200

    Add modal, add var: --shadow

---
+++ index.js
@@ -0,0 +1,6 @@
+const addBookBtn = document.querySelector("#addBookBtn");
+const modal = document.querySelector("dialog");
+
+addBookBtn.addEventListener("click", (e) => {
+    modal.showModal();
+});
