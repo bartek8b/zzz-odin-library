10d8a845b2e0b31bed08819ef779f8a071161fe9
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-05-24 21:01:58 +0200

    Set addBookToLibrary func, test push to [] - ok

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const addBookBtn = document.querySelector("#addBookBtn");
 const modal = document.querySelector("dialog");
+const cancelBtn = document.querySelector("#cancelBtn");
+const submitBtn = document.querySelector("#submitBtn");
 
-addBookBtn.addEventListener("click", (e) => {
+const myLibrary = [];
+
+function Book(title, author, pages, read, id){
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = id;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const id = self.crypto.randomUUID();
+  const book = new Book(title, author, pages, read, id);
+  myLibrary.push(book);
+}
+
+
+// EVENT LISTENERS
+
+addBookBtn.addEventListener("click", () => {
     modal.showModal();
 });
+
+cancelBtn.addEventListener("click", () => {
+    modal.close();
+})
+
+submitBtn.addEventListener("click", () => {
+    
+})
+
+// addBookToLibrary("fsdfsdf", "author", "pages", "read");
+// console.log(myLibrary);
